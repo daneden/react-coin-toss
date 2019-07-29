549b8db01a10ebaff0766364f3cf48735aa4da21
Daniel Eden <dan.eden@me.com>
2019-07-29 10:56:58 -0700

    Step 2: Add the "Toss Coin" button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react"
 
+const { useState } = React
+
 function App() {
-  return <div>The coin toss app will go here!</div>
+  const [side, setSide] = useState(1)
+  const [tossed, setTossed] = useState(0)
+
+  const tossCoin = () => {
+    const landedOn = Math.round(Math.random())
+    setSide(landedOn)
+    setTossed(tossed + 1)
+  }
+
+  return (
+    <div>
+      <p>The coin has been tossed {tossed} times.</p>
+      <p>It landed on {side === 1 ? "heads" : "tails"}</p>
+      <button onClick={tossCoin}>Toss coin</button>
+    </div>
+  )
 }
 
 export default App
