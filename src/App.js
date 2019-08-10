9006016cd3fb3a56d5961140db92e14c5ae19d74
Daniel Eden <dan.eden@me.com>
2019-08-10 16:36:48 +0100

    Add visualisation of each side and automatically toss the coin.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import React from "react"
 
-const { useState } = React
+const { useEffect, useState } = React
 
 function App() {
   const [side, setSide] = useState(1)
-  const [tossed, setTossed] = useState(0)
+  const [heads, setHeads] = useState(0)
+  const [tails, setTails] = useState(0)
+  const [isPaused, setIsPaused] = useState(false)
+
+  const tossed = heads + tails
 
   const tossCoin = () => {
     const landedOn = Math.round(Math.random())
+
+    if (landedOn === 1) {
+      setHeads(heads + 1)
+    } else {
+      setTails(tails + 1)
+    }
+
     setSide(landedOn)
-    setTossed(tossed + 1)
   }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!isPaused) {
+        tossCoin()
+      }
+    }, 10)
+    return () => clearInterval(interval)
+  })
 
   return (
     <div>
       <p>The coin has been tossed {tossed} times.</p>
       <p>It landed on {side === 1 ? "heads" : "tails"}</p>
-      <button onClick={tossCoin}>Toss coin</button>
+      <ul>
+        <li>
+          <label htmlFor="heads">Heads: {heads}</label>
+          <meter id="heads" value={heads} max={tossed} />
+        </li>
+        <li>
+          <label htmlFor="tails">Tails: {tails}</label>
+          <meter id="tails" value={tails} max={tossed} />
+        </li>
+      </ul>
+      <button onClick={() => setIsPaused(!isPaused)}>
+        {!isPaused ? "Pause" : "Continue"}
+      </button>
+      {isPaused && <button onClick={tossCoin}>Toss coin</button>}
     </div>
   )
 }
